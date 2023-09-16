701fa67cf9d45711313b5ed30175ee79ac70c8a8
Asraf071198 <asrafjahan98@gmail.com>
2023-09-16 19:16:28 +0530

    BLOOD BANK

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+
+const usersRoute = require("./routes/usersRoute");
+const inventoryRoute = require("./routes/inventoryRoute");
+const dashboardRoute = require("./routes/dashboardRoute");
+
+app.use("/api/users", usersRoute);
+app.use("/api/inventory", inventoryRoute);
+app.use("/api/dashboard", dashboardRoute);
+
+
+
+app.listen(port, () => console.log(`Node JS Server Started at ${port}`));
